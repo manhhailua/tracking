d025b4fa57e24b0b17594b05a5b1450e14a214fe
manhpt <manhpt@phi.vn>
2016-06-02 11:48:59 +0700

    disable DEBUG

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -45,7 +45,6 @@
 
     new Fingerprint2().get(function (result, components) {
       console.log('fp: ', result); // Current fingerprint
-      document.getElementsByName('fingerPrint')[0].value = result;
       TRequest.imgGet('//manhhailua.herokuapp.com/users?fp=' + result, function () {
         console.log('Tracking request has sent with fingerprint: ', result);
       });
